b30a93d55a5f122748b01a90bd78c35a6defcf7f
ehsanhusaini <ehsan@bluebits.org>
2023-12-13 00:42:37 +0330

    Add project files.

---
+++ StateMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StepStateMachine
+{
+    public enum State
+    {
+        Deraft = 1,
+        Done = 2,
+        Error = 3
+    }
+
+    public interface IRule
+    {
+        Task<TEntity> Run<TEntity>(TEntity entity) where TEntity : class, IStatebleEntity;
+        Task<TEntity> Reverse<TEntity>(TEntity entity) where TEntity : class, IStatebleEntity;
+    }
+
+    public interface IStatebleEntity
+    {
+        public State State { get; set; }
+    }
+
+    public abstract class StepStateMachine<TEntity>
+        where TEntity : class, IStatebleEntity
+    {
+
+    }
+}
